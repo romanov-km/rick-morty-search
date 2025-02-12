33091be4a7c6ab67bbd90dc283c4619a52d8d092
Kirill Romanov <workrkm@mail.ru>
2025-02-12 04:30:27 +0300

    Fixed GitHub Pages routing and console clearing

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/rick-morty-search/",
+  server: {
+    open: true,
+    clearScreen: false, // Отключает очистку консоли
+  },
 })
